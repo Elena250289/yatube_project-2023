f0195ceffb6b4410184aa1c6343bb606d616caed
Елена <alenazah1989@mail.ru>
2023-06-16 19:40:28 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse('Посты о постах')
