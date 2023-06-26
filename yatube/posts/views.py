31d7c0ababd3325ecdd29be25525c76e38126d0d
Елена <alenazah1989@mail.ru>
2023-06-26 17:09:41 +0300

    Подключение CSS

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    title = 'Главная страница'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': "Это главная страница проекта Yatube",
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse('Посты о постах')
+    template = 'posts/group_list.html'
+    title = 'Главная страница'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': "Здесь будет информация о группах проекта Yatube",
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
