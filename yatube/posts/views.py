8427c952ff50d0c679cbd1b199aacb08420b5239
Елена <alenazah1989@mail.ru>
2023-06-28 10:05:03 +0300

    Вывод информации из базы(без задания)

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,19 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Post
 
-# Главная страница
 def index(request):
-    template = 'posts/index.html'
-    title = 'Главная страница'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': "Это главная страница проекта Yatube",
+        'posts': posts,
     }
-    # Третьим параметром передаём словарь context
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 # Страница со списком мороженого
